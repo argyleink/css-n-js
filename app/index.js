8eead858f46a623cf70d477e6126a43e85100696
Adam Argyle <atom@argyleink.com>
2019-09-11 13:06:57 -0700

    direct selector chain

--- app/index.js
+++ app/index.js
@@ -10,3 +10,12 @@ experiments
       padding: '.5rem 1rem',
       boxShadow: '0 2rem 1.5rem -1rem hsla(0, 0%, 0%, 15%)',
     }))
+
+experiments
+  .flatMap(experiment => [...experiment.children])
+  .filter(child => child.nodeName === 'H2')
+  .forEach(({style}) => 
+    Object.assign(style, {
+      textDecoration: 'underline',
+      textDecorationColor: 'hsl(200 82% 45%)',
+    }))
