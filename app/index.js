b976be662a91a6f6704aeb11f1e251e68ccf2aa1
Adam Argyle <atom@argyleink.com>
2019-09-12 14:36:30 -0700

    ending demo

--- app/index.js
+++ app/index.js
@@ -10,3 +10,31 @@ experiments
       padding: '.5rem 1rem',
       boxShadow: '0 2rem 1.5rem -1rem hsla(0, 0%, 0%, 15%)',
     }))
+
+experiments
+  .flatMap(experiment => [...experiment.children])
+  .filter(child => child.nodeName === 'P')
+  .map(node => {
+    const {0:first, length:l, [l - 1]:last} = 
+      [...node.parentElement.children]
+      .filter(child => child.nodeName === 'P')
+
+    return {node, first, last}
+  })
+  .filter(({node, first, last}) => 
+    node !== first && node !== last)
+  .map(({node}) => node)
+  .forEach(({style}) => 
+    Object.assign(style, {
+      textDecoration: 'underline',
+      textDecorationColor: 'hsl(200 82% 45%)',
+    }))
+
+experiments
+  .flatMap(experiment => [...experiment.children])
+  .filter(child => child.nodeName === 'P')
+  .filter(child => !child.nextElementSibling)
+  .forEach(({style}) => 
+    Object.assign(style, {
+      textTransform: 'uppercase',
+    }))
