24b19dbed52f55ac8a0ddb7d2484b0a4898f6c73
Adam Argyle <atom@argyleink.com>
2019-09-18 15:24:33 -0700

    update to latest

--- app/js/experiment.js
+++ app/js/experiment.js
@@ -1,31 +1,31 @@
+import { assignStyle, directDescendants, byAttr, lastChild, firstChild } from './utils.js'
+
 const experiments = [...document.querySelectorAll('.🤓')]
 
 // .🤓
 experiments
-  .forEach(({style}) => 
-    Object.assign(style, {
-      fontFamily: 'sans-serif',
-      backgroundColor: 'hsl(200  100% 90%)',
-      color: 'hsl(200 82% 15%)',
-      borderRadius: '1rem',
-      padding: '.5rem 1rem',
-      boxShadow: '0 2rem 1.5rem -1rem hsla(0, 0%, 0%, 15%)',
-    }))
+  .forEach(assignStyle({
+    fontFamily: 'sans-serif',
+    backgroundColor: 'hsl(200  100% 90%)',
+    color: 'hsl(200 82% 15%)',
+    borderRadius: '1rem',
+    padding: '.5rem 1rem',
+    boxShadow: '0 2rem 1.5rem -1rem hsla(0, 0%, 0%, 15%)',
+  }))
 
 // .🤓 > h2
 experiments
-  .flatMap(experiment => [...experiment.children])
-  .filter(child => child.nodeName === 'H2')
-  .forEach(({style}) =>
-    Object.assign(style, {
-      textDecoration: 'underline',
-      textDecorationColor: 'hsl(200 82% 45%)',
-    }))
+  .flatMap(directDescendants)
+  .filter(byAttr('nodeName', 'h2'))
+  .forEach(assignStyle({
+    textDecoration: 'underline',
+    textDecorationColor: 'hsl(200 82% 45%)',
+  }))
 
 // .🤓 > p:not(:first-of-type):not(:last-of-type)
 experiments
-  .flatMap(experiment => [...experiment.children])
-  .filter(child => child.nodeName === 'P')
+  .flatMap(directDescendants)
+  .filter(byAttr('nodeName', 'p'))
   .map(node => {
     const {0:first, length:l, [l - 1]:last} = 
       [...node.parentElement.children]
@@ -36,18 +36,16 @@ experiments
   .filter(({node, first, last}) =>
     node !== first && node !== last)
   .map(({node}) => node)
-  .forEach(({style}) => 
-    Object.assign(style, {
-      textDecoration: 'underline',
-      textDecorationColor: 'hsl(200 82% 45%)',
-    }))
+  .forEach(assignStyle({
+    textDecoration: 'underline',
+    textDecorationColor: 'hsl(200 82% 45%)',
+  }))
 
 // .🤓 > p:last-child
 experiments
-  .flatMap(experiment => [...experiment.children])
-  .filter(child => child.nodeName === 'P')
-  .filter(node => !node.nextElementSibling)
-  .forEach(({style}) => 
-    Object.assign(style, {
-      textTransform: 'uppercase',
-    }))
+  .flatMap(directDescendants)
+  .filter(byAttr('nodeName', 'p'))
+  .filter(lastChild)
+  .forEach(assignStyle({
+    textTransform: 'uppercase',
+  }))
